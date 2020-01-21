a17fc5d0b7f0edf1ab73d1703e27f777f5e939a1
antosha-85 <antosha-85@yandex.ru>
2020-01-21 02:15:15 +0000

    Finished new post rout and added an alfanumeric generator function

--- express_server.js
+++ express_server.js
@@ -1,13 +1,31 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+   let result           = '';
+   let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   let charactersLength = characters.length;
+   for ( let i = 0; i < 6; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
+}
+
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -34,17 +52,21 @@ app.get("/fetch", (req, res) => {
 // routes
 
 app.get ('/urls', (req, res) => {
-    let templateVars = { urls: urlDatabase };
-    res.render("urls_index", templateVars);
+    let templatelets = { urls: urlDatabase };
+    res.render("urls_index", templatelets);
 })
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
-    let templateVars = 
+    let templatelets = 
     { 
       shortURL: req.params.shortURL, 
       longURL: urlDatabase[req.params.shortURL]
     };
-    res.render('urls_show', templateVars)
+    res.render('urls_show', templatelets)
     
   })  
 app.listen(PORT, () => {
