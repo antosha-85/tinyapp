2c34f0ab8dfe50bdfddf816b74bbf623bcc636aa
antosha-85 <antosha-85@yandex.ru>
2020-01-21 00:36:17 +0000

    Finished part with short and long urls

--- express_server.js
+++ express_server.js
@@ -25,12 +25,28 @@ res.send("<html><body>Hello <b>World1</b></body></html>\n");
 app.get("/set", (req, res) => {
     const a = 1;
     res.send(`a = ${a}`);
-   });
+});
    
 app.get("/fetch", (req, res) => {
     res.send(`a = ${a}`);
 });
-   
+
+// routes
+
+app.get ('/urls', (req, res) => {
+    let templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+})
+
+app.get("/urls/:shortURL", (req, res) => {
+    let templateVars = 
+    { 
+      shortURL: req.params.shortURL, 
+      longURL: urlDatabase[req.params.shortURL]
+    };
+    res.render('urls_show', templateVars)
+    
+  })  
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
